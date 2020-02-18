9d3a4344a5cae8293d7e2192392ac61f07f0ea3d
RemyHennessy <vhbattad@gmail.com>
2020-02-18 16:31:12 +0800

    update

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
+        ConsoleTVs\Charts\ChartsServiceProvider::class,
       
 
         /*
